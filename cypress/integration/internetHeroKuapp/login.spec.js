325fb268c059abf09fae6304364c5ae8151f9683
Azeez Lameed <azeezlameed@gmail.com>
2021-12-31 18:38:23 +0100

    first commit

--- cypress/integration/internetHeroKuapp/login.spec.js
+++ cypress/integration/internetHeroKuapp/login.spec.js
@@ -1,26 +1,44 @@
 /// <reference types='cypress' />
 
-it('should be able to login', function () {
+describe('Authentication', function() {
+    beforeEach( function(){
     cy.visit('http://the-internet.herokuapp.com/login')
 
     // assert login page
     cy.get('h2').should('contain.text', 'Login Page')
+    })
+    it('should be able to login', function () {
     
-    //input username
-    cy.get('#username').type('tomsmith')
-    //input password
-    cy.get('#password').type('SuperSecretPassword!')
+        //input username
+        cy.get('#username').type('tomsmith')
+        //input password
+        cy.get('#password').type('SuperSecretPassword!')
+    
+        //click login button
+        cy.get('.radius').click()
+    
+        //assert login was successful
+        cy.get('#flash').should('contain.text', 'You logged into a secure area!') 
+    
+        //Log out of the application
+        cy.get('.button').click()
 
-    //click login button
-    cy.get('.radius').click()
+        //assert user logged out successfully
+        cy.get('#flash').should('contain.text', 'You logged out of the secure area!')
+    })
 
-    //assert login was successful
-    cy.get('#flash').should('contain.text', 'You logged into a secure area!')
+        // unsuccessful login
+        it('should not be able to login', function () {
 
-    //Log out of the application
-    cy.get('.button').click()
+        // No username
+        cy.get('#username').type('tome')
+        //input password
+        cy.get('#password').type('SuperSecretPassword!')
 
-    //assert user logged out successfully
-    cy.get('#flash').should('contain.text', 'You logged out of the secure area!')
+        //click login button
+        cy.get('.radius').click()
 
-} )
+        //assert login error message
+        cy.get('#flash').should('contain.text', 'Your username is invalid!')
+    })
+})
